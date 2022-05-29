4ba1c4dee7e0feb9fc585c2c57945b00ce35d910
genaro14 <genaropennone@gmail.com>
2022-05-29 19:06:49 -0300

    dotENV usage

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 const indexPath = __dirname + '/views/index.html';
 const publicPath = __dirname + '/public';
-const responseJSON = { "message": "Hello json" };
+
 
 console.log("Hello World");
 app.use("/public", express.static(publicPath));
@@ -11,8 +12,14 @@ app.get('/', function (req, res) {
     res.sendFile(indexPath)
 });
 
+console.log(process.env.MESSAGE_STYLE);
 app.get('/json', function (req, res) {
-    res.json(responseJSON)
+    let response = {};
+    const lowercase = { "message": "Hello json" };
+    const uppercase = { "message": "HELLO JSON" };
+    if (process.env.MESSAGE_STYLE == 'uppercase') response = uppercase
+    else response = lowercase;
+    res.json(response)
 });
 
 
