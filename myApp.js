1328a27b43a6bc842f3b27dcf98347eb5afc6fef
genaro14 <genaropennone@gmail.com>
2022-05-29 18:45:17 -0300

    Static Assets

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
 let express = require('express');
 let app = express();
-const absolutePath = __dirname + '/views/index.html'
+const indexPath = __dirname + '/views/index.html';
+const publicPath = __dirname + '/public';
+var path = require('path');
 
 console.log("Hello World");
+app.use("/public", express.static(publicPath));
 app.get('/',function(req, res){
-    res.sendFile(absolutePath)
+    res.sendFile(indexPath)
 });
-
 
 
 
