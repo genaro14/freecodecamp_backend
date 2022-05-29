7a7880b12b7d9e4ac13413d833ce5e4a945a1ce3
genaro14 <genaropennone@gmail.com>
2022-05-29 18:54:05 -0300

    JSON response

--- myApp.js
+++ myApp.js
@@ -2,12 +2,17 @@ let express = require('express');
 let app = express();
 const indexPath = __dirname + '/views/index.html';
 const publicPath = __dirname + '/public';
-var path = require('path');
+const responseJSON = { "message": "Hello json" };
 
 console.log("Hello World");
 app.use("/public", express.static(publicPath));
-app.get('/',function(req, res){
+
+app.get('/', function (req, res) {
     res.sendFile(indexPath)
+});
+
+app.get('/json', function (req, res) {
+    res.json(responseJSON)
 });
 
 
@@ -44,4 +49,5 @@ app.get('/',function(req, res){
 
 
 
- module.exports = app;
+
+module.exports = app;
