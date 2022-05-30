419d48367eaf66fa94775f34a24fb08ae6ca54d3
genaro14 <genaropennone@gmail.com>
2022-05-30 19:11:28 -0300

    Body parser

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ let app = express();
 require('dotenv').config()
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 const indexPath = __dirname + '/views/index.html';
 const publicPath = __dirname + '/public';
 
